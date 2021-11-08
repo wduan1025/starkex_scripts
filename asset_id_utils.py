a9edb4528478d6ad3f4b36e8651bbcdec8ddf6ab
Wei Duan <weiduan1025@gmail.com>
2021-11-08 18:19:24 -0500

    FIx bugs in this script

--- asset_id_utils.py
+++ asset_id_utils.py
@@ -12,24 +12,23 @@ def getErcMintable721AssetInfo(address):
 def getMintableErc721AssetType(address):
    asset_info = getErcMintable721AssetInfo(address)
    print('asset info is', asset_info)
-   print(Web3.solidityKeccak(["bytes", "uint256"], [asset_info, 1]))
-   asset_type = Web3.solidityKeccak(asset_info, 1) & 0x03FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+   asset_type = Web3.solidityKeccak(["bytes", "uint256"], [asset_info, 1])
    return asset_type
 
 
 def getMintableErc721AssetId(minting_blob, address):
-   blob_hash = Web3.solidityKeccak(["string"], [minting_blob])
+   blob_hash = Web3.solidityKeccak(["bytes"], [minting_blob])
    asset_type = getMintableErc721AssetType(address)
-   hash = Web3.solidityKeccak(["string", "uint256", "bytes"], ['MINTABLE:', asset_type, blob_hash])
+   hash = Web3.solidityKeccak(["string", "bytes", "bytes"], ['MINTABLE:', asset_type, blob_hash])
    asset_id = (int.from_bytes(hash, "big") & 0x0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) |  (1 << 250)
    return asset_id
 
 minting_blob = {
     'token_base_url': 'https://www.google.com',
-    'symbol': "xxx"
+    'symbol': "RNM"
 }
 
 minting_blob_bytes = json.dumps(minting_blob).encode('utf8')
 
 contract_address = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
-print('mintable erc721 asset id is', getMintableErc721AssetId(minting_blob, contract_address))
+print('mintable erc721 asset id is', getMintableErc721AssetId(minting_blob_bytes, contract_address))
